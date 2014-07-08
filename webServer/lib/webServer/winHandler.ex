c61ff6991d7620d2f9c138cbc50edc19cab898f9
NikolaMandic <mn080202@gmail.com>
2014-07-08 08:38:12 +0200

    win notification

--- webServer/lib/webServer/winHandler.ex
+++ webServer/lib/webServer/winHandler.ex
@@ -16,24 +16,41 @@ defmodule WebServer.WinHandler do
 
   end
   def handle(req,state) do
-  :gen_event.call(:"money",{:registerWin})
-  auctionID=false
-  {auctionID, Req2} = :cowboy_req.qs_val("auctionID", req)
-  if auctionID do
-  Agent.update(:inFlight, fn (d)-> 
+  #:gen_event.call(:"money",{:registerWin})
+  #auctionID=false
+  #IO.puts(auctionID)
+  IO.puts("auctionID")
+
+  #{auction, req2}= :cowboy_req.qs_val('auctionid',req)
+  
+   case :cowboy_req.qs_vals( req) do
+                #{undefined, _} -> IO.puts("no echo_get querystring parameter")
+                #{x, _} -> IO.puts(x)
+		{x,req2} -> 
+			 [{<<"auctionID">>,auctionIDR},{<<"priceWon">>,priceWon}]=x
+			 auctionID= auctionIDR
+ 			 priceWon=String.to_integer( priceWon)
+			 
+            end
+  
+  IO.puts( auctionID)
+  IO.puts(priceWon)
+  #if auctionID do
+  Agent.update(:inFlight, fn (d)->
+    IO.puts("asd")
     priceInFlight=Dict.get(auctionID)
-    ##now send to money manager
-    :gen_event.call(:money,{:registerWin,priceInFlight})
+  #  ##now send to money manager
+    #:gen_event.call(:money,{:registerWin,priceInFlight,priceWon})
     Dict.delete(d, auctionID)
-end
+   end
     
     )
-   end 
-  IO.puts :cowboy.body(req)
+  # end 
+  #IO.puts :cowboy.body(req)
   #decide
-
+  IO.puts("endR")
   {:ok,req} = :cowboy_req.reply(200,[],"asdasd",req)
-  {:ok,req,:state}
+  {:ok,req,state}
   end
   def terminate(_r,_e,s) do
   {:ok}
